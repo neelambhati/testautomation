c88f25ba082100d52ea47f0bbd7e5922a4179b8e
neelambhati <neelambhati.69@gmail.com>
2025-01-05 16:29:57 +0530

    api automation

---
+++ src/com/api/apiTest.java
@@ -0,0 +1,26 @@
+package com.api;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasKey;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class apiTest {
+
+	public static void main(String[] args) {
+		Response response = RestAssured.given()
+	            .baseUri("https://api.coindesk.com/v1/bpi/currentprice.json").get();
+	        response.then().statusCode(200);
+		
+	// Verify the response contains the expected BPIs (USD, GBP, EUR)
+    response.then().body("bpi", hasKey("USD"));
+    response.then().body("bpi", hasKey("GBP"));
+    response.then().body("bpi", hasKey("EUR"));
+
+    // Verify the 'GBP' description equals 'British Pound Sterling'
+    response.then().body("bpi.GBP.description", equalTo("British Pound Sterling"));
+
+	}
+
+}
